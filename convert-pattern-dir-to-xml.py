c20e35ffce9ef492c2dfb6609e41129b9b5f26a0
Marko Saukko <marko.saukko@cybercom.com>
2011-12-12 10:44:53 +0200

    Added support for old obs xml format.

--- convert-pattern-dir-to-xml.py
+++ convert-pattern-dir-to-xml.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
 
+# This script is used to convert .yaml files to patterns and package groups.
+# Copyright (C) 2011 Marko Saukko <FIRSTNAME.LASTNAME@gmail.com>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# This script is based on MeeGo package-group scripts
+# https://meego.gitorious.org/meego-os-base/package-groups/
+
 import yaml
 import sys, os
 import optparse
@@ -13,19 +33,21 @@ NSMAP_GROUP = {None : pattern_ns, "rpm": rpm_ns, "patterns": pattern_ns}
 
 TMP_FILE = "xmllint_tmp.xml"
 
-def process_yaml(stream, proot):
+def process_yaml(stream, proot, newobsapi):
 	y = yaml.load(stream)
 
 	# <name>
 	etree.SubElement(proot, "name").text = y['Name']
 	
-	# <version>
-	if y.has_key('Version'):
-		etree.SubElement(proot, "version").attrib['ver'] = "%s" % y['Version']
-	
-	# <arch>
-	if y.has_key('Arch'):
-		etree.SubElement(proot, "arch").text = "%s" % y['Arch']
+	# Old OBS isn't able to handle these options.
+	if newobsapi:
+		# <version>
+		if y.has_key('Version'):
+			etree.SubElement(proot, "version").attrib['ver'] = "%s" % y['Version']
+		
+		# <arch>
+		if y.has_key('Arch'):
+			etree.SubElement(proot, "arch").text = "%s" % y['Arch']
 	
 	# <summary>
 	etree.SubElement(proot, "summary").text = y['Summary']
@@ -54,7 +76,7 @@ def process_yaml(stream, proot):
 	
 	return proot
 
-def create_patterns(patterns_dir, outputdir):
+def create_patterns(patterns_dir, outputdir,newobsapi):
 	for f in os.listdir(patterns_dir):
 		if not f.endswith('.yaml'):
 			continue
@@ -65,31 +87,30 @@ def create_patterns(patterns_dir, outputdir):
 		
 		stream = file("%s/%s" %(patterns_dir,f), 'r')
 		proot = etree.Element("pattern",  nsmap=NSMAP)
-		proot = process_yaml(stream,proot)
+		proot = process_yaml(stream,proot,newobsapi)
 
 		# Indent the XML with xmllint and output to file.
 		tree = etree.ElementTree(proot)
 		tree.write("%s" % (TMP_FILE))
 		os.system("xmllint --format %s --output %s" % (TMP_FILE, output_file))
 
-def merge_patterns(patterns_dir,outputdir):
+def merge_patterns(patterns_dir,outputdir,newobsapi):
 	xmlroot = etree.Element("patterns")
-
+	output_file = "%s/group.xml" % (outputdir)
 	count = 0
 	for f in os.listdir(patterns_dir):
 		if not f.endswith('.yaml'):
 			continue
-		print >> sys.stderr, "Working on %s" %f
+		print "Merging %s to %s." % (f,output_file)
 		count = count + 1
 		stream = file("%s/%s" %(patterns_dir,f), 'r')
 		proot = etree.SubElement(xmlroot, "pattern",  nsmap=NSMAP_GROUP)
-		proot = process_yaml(stream,proot)
+		proot = process_yaml(stream,proot,newobsapi)
 
 	xmlroot.set('count', "%d" %count)
 	tree = etree.ElementTree(xmlroot)
 	# Indent the XML with xmllint and output to file.
-	tree.write("%s" % (TMP_FILE))
-	output_file = "%s/group.xml" % (outputdir)
+	tree.write("%s" % (TMP_FILE))	
 	os.system("xmllint --format %s --output %s" % (TMP_FILE, output_file))
 
 if __name__ == '__main__':
@@ -107,8 +128,15 @@ if __name__ == '__main__':
 	parser.add_option("-o", "--outputdir", type="string", dest="outputdir",
 			default=".",
 			help="Output directory where the resulting .xml files are created.")
-		
+	parser.add_option("", "--old-obs-xml-format", action="store_false", dest="newobsapi",
+			default=True,
+			help="The old OBS api isn't able to handle the newer xml format.")
+	
 	(options, args) = parser.parse_args()
+	
+	if (not options.groupxml and not options.patternxml):
+		print "Error: you need to use either --patternxml or --groupxml."
+		exit(1)
 	
 	if (not options.patterndir or not os.path.exists(options.patterndir)):
 		print "Error: Pattern dir '%s' doesn't exist." % (options.patterndir)
@@ -118,7 +146,7 @@ if __name__ == '__main__':
 		os.makedirs(options.outputdir)
 	
 	if options.patternxml:
-		create_patterns(options.patterndir,options.outputdir)
+		create_patterns(options.patterndir,options.outputdir, options.newobsapi)
 
 	if options.groupxml:
-		merge_patterns(options.patterndir,options.outputdir)
+		merge_patterns(options.patterndir,options.outputdir, options.newobsapi)
